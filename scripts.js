b182efaeb1eede58c018a351fbfb68db486dedb1
jeffersbaxter <jeffersbaxter@comcast.net>
2016-01-02 22:50:54 -0800

    no js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function(){
-
-	$(".nav a").on("click", function(){
-	   $(".nav").find(".active").removeClass("active");
-	   $(this).parent().addClass("active");
-	});
-// });
